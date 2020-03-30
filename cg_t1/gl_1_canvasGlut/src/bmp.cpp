14465efcbdbfe37dc7f1c8f6c520a884e5410a2c
Augusto Gai Dal'Asta <agasta@inf.ufsm.br>
2020-03-30 18:51:48 -0300

    Add files via upload

---
+++ cg_t1/gl_1_canvasGlut/src/bmp.cpp
@@ -0,0 +1,400 @@
+//*********************************************************
+//
+// classe para fazer o carregamento de arquivos no formato BMP
+// Autor: Cesar Tadeu Pozzer
+//        pozzer@inf.ufsm.br
+//  Versao 09/2010
+//
+//**********************************************************
+
+#include "Bmp.h"
+#include <string.h>
+#include <stdbool.h>
+#include "gl_canvas2d.h"
+
+Bmp::Bmp(const char *fileName)
+{
+   width = height = 0;
+   data = NULL;
+
+   if( fileName != NULL && strlen(fileName) > 0 )
+   {
+      load(fileName);
+      printf("\nFile opened.");
+   }
+   else
+   {
+      printf("Error: Invalid BMP filename");
+   }
+
+   startCount();
+   printf("\nStart arrays.");
+
+   countColors();
+   printf("\nColors counted.");
+
+   countLum();
+   printf("\nLuminosity counted.");
+}
+
+uchar* Bmp::getImage()
+{
+  return data;
+}
+
+int Bmp::getWidth(void)
+{
+  return width;
+}
+
+int Bmp::getHeight(void)
+{
+  return height;
+}
+
+void Bmp::convertBGRtoRGB()
+{
+  unsigned char tmp;
+  if( data != NULL )
+  {
+     for(int y=0; y<height; y++)
+     for(int x=0; x<width*3; x+=3)
+     {
+        int pos = y*bytesPerLine + x;
+        tmp = data[pos];
+        data[pos] = data[pos+2];
+        data[pos+2] = tmp;
+     }
+  }
+}
+
+
+void Bmp::load(const char *fileName)
+{
+  FILE *fp = fopen(fileName, "rb");
+  if( fp == NULL )
+  {
+     printf("\nErro ao abrir arquivo %s para leitura", fileName);
+     return;
+  }
+
+  printf("\n\nCarregando arquivo %s", fileName);
+
+  //le o HEADER componente a componente devido ao problema de alinhamento de bytes. Usando
+  //o comando fread(header, sizeof(HEADER),1,fp) sao lidos 16 bytes ao inves de 14
+  fread(&header.type,      sizeof(unsigned short int), 1, fp);
+  fread(&header.size,      sizeof(unsigned int),       1, fp);
+  fread(&header.reserved1, sizeof(unsigned short int), 1, fp);
+  fread(&header.reserved2, sizeof(unsigned short int), 1, fp);
+  fread(&header.offset,    sizeof(unsigned int),       1, fp);
+
+  //le o INFOHEADER componente a componente devido ao problema de alinhamento de bytes
+  fread(&info.size,        sizeof(unsigned int),       1, fp);
+  fread(&info.width,       sizeof(int),                1, fp);
+  fread(&info.height,      sizeof(int),                1, fp);
+  fread(&info.planes,      sizeof(unsigned short int), 1, fp);
+  fread(&info.bits,        sizeof(unsigned short int), 1, fp);
+  fread(&info.compression, sizeof(unsigned int),       1, fp);
+  fread(&info.imagesize,   sizeof(unsigned int),       1, fp);
+  fread(&info.xresolution, sizeof(int),                1, fp);
+  fread(&info.yresolution, sizeof(int),                1, fp);
+  fread(&info.ncolours,    sizeof(unsigned int),       1, fp);
+  fread(&info.impcolours,  sizeof(unsigned int),       1, fp);
+
+  width  = info.width;
+  height = info.height;
+  bits   = info.bits;
+  bytesPerLine =(3 * (width + 1) / 4) * 4;
+  imagesize    = bytesPerLine*height;
+  int delta    = bytesPerLine - (3 * width);
+
+  printf("\nImagem: %dx%d - Bits: %d", width, height, bits);
+  printf("\nbytesPerLine: %d", bytesPerLine);
+  printf("\nbytesPerLine: %d", width * 3);
+  printf("\ndelta: %d", delta);
+  printf("\nimagesize: %d %d", imagesize, info.imagesize);
+
+  if( header.type != 19778 )
+  {
+     printf("\nError: Arquivo BMP invalido");
+     getchar();
+     exit(0);
+  }
+
+  if( width*height*3 != imagesize )
+  {
+     printf("\nWarning: Arquivo BMP nao eh potencia de 2");
+     getchar();
+  }
+
+  if( info.compression != 0 )
+  {
+     printf("\nError: Formato BMP comprimido nao suportado");
+     getchar();
+     return;
+  }
+  if( bits != 24 )
+  {
+     printf("\nError: Formato BMP com %d bits/pixel nao suportado", bits);
+     getchar();
+     return;
+  }
+
+  if( info.planes != 1 )
+  {
+     printf("\nError: Numero de Planes nao suportado: %d", info.planes);
+     getchar();
+     return;
+  }
+
+  data = new unsigned char[imagesize];
+  fread(data, sizeof(unsigned char), imagesize, fp);
+
+  fclose(fp);
+}
+
+void Bmp::startCount()
+{
+    r_max = 0;
+    g_max = 0;
+    b_max = 0;
+    l_max = 0;
+
+    for(int i = 0; i < 256; i++) {
+        r_count[i] = 0;
+        g_count[i] = 0;
+        b_count[i] = 0;
+        l_count[i] = 0;
+    }
+}
+
+void Bmp::renderBitmap(int pos_x, int pos_y, bool r, bool g, bool b, int rotation)
+{
+    clear(0,0,0);
+
+    int sum = 0;
+
+    if(rotation == 1) {
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                float red = ((float)data[sum]/255) * r;
+                float green = ((float)data[sum + 1]/255) * g;
+                float blue = ((float)data[sum + 2]/255) * b;
+
+                color(red, green, blue);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 2) {
+        for(int y = height; y > 0; y--) {
+            for(int x = width; x > 0; x--) {
+                float red = ((float)data[sum]/255) * r;
+                float green = ((float)data[sum + 1]/255) * g;
+                float blue = ((float)data[sum + 2]/255) * b;
+
+                color(red, green, blue);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 3) {
+        for(int x = width; x > 0; x--) {
+            for(int y = 0; y < width; y++) {
+                float red = ((float)data[sum]/255) * r;
+                float green = ((float)data[sum + 1]/255) * g;
+                float blue = ((float)data[sum + 2]/255) * b;
+
+                color(red, green, blue);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 4) {
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < width; y++) {
+                float red = ((float)data[sum]/255) * r;
+                float green = ((float)data[sum + 1]/255) * g;
+                float blue = ((float)data[sum + 2]/255) * b;
+
+                color(red, green, blue);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    }
+}
+
+void Bmp::renderBitmapMonochrome(int pos_x, int pos_y, int rotation)
+{
+    clear(0,0,0);
+
+    int sum = 0;
+
+    if(rotation == 1) {
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                float red = ((float)data[sum]/255) * 0.299;
+                float green = ((float)data[sum + 1]/255) * 0.587;
+                float blue = ((float)data[sum + 2]/255) * 0.114;
+
+                float lum = red + green + blue;
+
+                color(lum, lum, lum);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 2) {
+        for(int y = height; y > 0; y--) {
+            for(int x = width; x > 0; x--) {
+                float red = ((float)data[sum]/255) * 0.299;
+                float green = ((float)data[sum + 1]/255) * 0.587;
+                float blue = ((float)data[sum + 2]/255) * 0.114;
+
+                float lum = red + green + blue;
+
+                color(lum, lum, lum);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 3) {
+        for(int x = width; x > 0; x--) {
+            for(int y = 0; y < width; y++) {
+                float red = ((float)data[sum]/255) * 0.299;
+                float green = ((float)data[sum + 1]/255) * 0.587;
+                float blue = ((float)data[sum + 2]/255) * 0.114;
+
+                float lum = red + green + blue;
+
+                color(lum, lum, lum);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    } else if (rotation == 4) {
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < width; y++) {
+                float red = ((float)data[sum]/255) * 0.299;
+                float green = ((float)data[sum + 1]/255) * 0.587;
+                float blue = ((float)data[sum + 2]/255) * 0.114;
+
+                float lum = red + green + blue;
+
+                color(lum, lum, lum);
+                point(pos_x + x, pos_y + y);
+
+                sum += 3;
+            }
+        }
+    }
+}
+
+
+void Bmp::countColors()
+{
+    int sum = 0;
+
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            r_count[data[sum]]++;
+            g_count[data[sum + 1]]++;
+            b_count[data[sum + 2]]++;
+
+            sum += 3;
+        }
+    }
+
+    for(int i = 0; i < 256; i++) {
+        if(r_count[i] > r_max) r_max = r_count[i];
+        if(g_count[i] > g_max) g_max = g_count[i];
+        if(b_count[i] > b_max) b_max = b_count[i];
+    }
+}
+
+void Bmp::renderHistogram(int x0, int y0, int xf, int yf, bool r, bool g, bool b)
+{
+    graph(x0, y0, xf, yf);
+
+    for(int x = 0; x < 256; x++) {
+        color(1, 0, 0);
+        int r_pos = (int)round((r_count[x] * 100)/r_max);
+        if(r == true) point(x0 + x, y0 + r_pos);
+
+        color(0, 1, 0);
+        int g_pos = (int)round((g_count[x] * 100)/g_max);
+        if(g == true) point(x0 + x, y0 + g_pos);
+
+        color(0, 0, 1);
+        int b_pos = (int)round((b_count[x] * 100)/b_max);
+        if(b == true) point(x0 + x, y0 + b_pos);
+    }
+}
+
+void Bmp::countLum()
+{
+    int sum = 0;
+
+    for(int y = 0; y < height; y++) {
+        for(int x = 0; x < width; x++) {
+            float red = ((float)data[sum]) * 0.299;
+            float green = ((float)data[sum + 1]) * 0.587;
+            float blue = ((float)data[sum + 2]) * 0.114;
+
+            float lum = red + green + blue;
+
+            l_count[(int)lum]++;
+
+            sum += 3;
+        }
+    }
+
+    for(int i = 0; i < 256; i++) {
+        if(l_count[i] > l_max) l_max = l_count[i];
+    }
+}
+
+void Bmp::renderHistogramMonochrome(int x0, int y0, int xf, int yf)
+{
+    graph(x0, y0, xf, yf);
+
+    for(int x = 0; x < 256; x++) {
+        color(1, 1, 1);
+        int l_pos = (int)round((l_count[x] * 100)/l_max);
+        point(x0 + x, y0 + l_pos);
+    }
+}
+
+void Bmp::graph(int x0, int y0, int xf, int yf)
+{
+    color(1, 1, 1);
+
+    text(x0 - 4, y0 - 16, "0");
+    text(x0 + ((xf - x0)/2) - 14, y0 -16, "128");
+    text(xf - 14, y0 - 16, "256");
+    for(int x = 0; x <= 256; x += 16) {
+        line(x0 + x, y0 - 2, x0 + x, y0 + 2);
+    }
+
+    line(x0, y0, xf + 8, y0);
+    line(xf + 4, y0 + 4, xf + 8, y0);
+    line(xf + 4, y0 - 4, xf + 8, y0);
+
+    line(x0, y0, x0, yf + 8);
+    line(x0 - 4, yf + 4, x0, yf + 8);
+    line(x0 + 4, yf + 4, x0, yf + 8);
+
+    text(x0 - 16, y0 - 4, "0");
+    text(x0 - 36, y0 + ((yf - y0)/2) - 4, "128");
+    text(x0 - 36, yf - 4, "256");
+    line(x0 - 4, y0 + ((yf - y0)/2), x0 + 4, y0 + ((yf - y0)/2));
+    line(x0 - 4, yf, x0 + 4, yf);
+}
---
+++ cg_t1/gl_1_canvasGlut/src/main.cpp
@@ -0,0 +1,223 @@
+/*********************************************************************
+// Canvas para desenho, criada sobre a API OpenGL. Nao eh necessario conhecimentos de OpenGL para usar.
+//  Autor: Cesar Tadeu Pozzer
+//         03/2020
+//
+//  Pode ser utilizada para fazer desenhos, animacoes, e jogos simples.
+//  Tem tratamento de mouse e teclado
+//  Estude o OpenGL antes de tentar compreender o arquivo gl_canvas.cpp
+//
+//  Instruções:
+//	  Para alterar a animacao, digite numeros entre 1 e 3
+// *********************************************************************/
+
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h> //callback da wheel do mouse.
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "gl_canvas2d.h"
+
+#include "Botao.h"
+#include "Bmp.h"
+
+Bmp *bmp = NULL;
+
+Botao *bf = NULL;
+Botao *br = NULL;
+Botao *bg = NULL;
+Botao *bb = NULL;
+Botao *bl = NULL;
+
+Botao *bup = NULL;
+Botao *bdown = NULL;
+Botao *bleft = NULL;
+Botao *bright = NULL;
+
+Botao *bplus = NULL;
+Botao *bminus = NULL;
+
+//variavel global para selecao do que sera exibido na canvas.
+int opcao  = 49;
+int auxOpcao = opcao;
+int rotation = 1;
+
+int screenWidth = 0, screenHeight = 0; //largura e altura inicial da tela . Alteram com o redimensionamento de tela.
+
+int x0, xf, y0, yf;
+
+int imgOffset, offset = 32, graphOffset = 256 + (2 * offset) + 8;
+int mouseX, mouseY; //variaveis globais do mouse para poder exibir dentro da render().
+
+void startButtons(int size_x, int size_y) {
+    bf = new Botao(x0, yf + offset, size_x, size_y, "F", 1, 1, 1);
+    br = new Botao(x0 + offset, yf + offset, size_x, size_y, "R", 1, 0, 0);
+    bg = new Botao(x0 + (2 * offset), yf +  offset, size_x, size_y, "G", 0, 1, 0);
+    bb = new Botao(x0 + (3 * offset), yf + offset, size_x, size_y, "B", 0, 0, 1);
+    bl = new Botao(x0 + (4 * offset), yf + offset, size_x, size_y, "M", 0.5, 0.5, 0.5);
+
+    bplus = new Botao(x0, yf + (2 * offset), size_x, size_y, "+", 1, 1, 1);
+    bminus = new Botao(x0 + offset, yf + (2 * offset), size_x, size_y, "-", 1, 1, 1);
+
+    bup = new Botao(x0 + (6.5 * offset), yf + (2 * offset), size_x, size_y, "^", 1, 1, 1);
+    bdown = new Botao(x0 + (6.5 * offset), yf + offset, size_x, size_y, "v", 1, 1, 1);
+    bleft = new Botao(x0 + (5.5 * offset), yf + offset, size_x, size_y, "<", 1, 1, 1);
+    bright = new Botao(x0 + (7.5 * offset), yf + offset, size_x, size_y, ">", 1, 1, 1);
+}
+
+void renderButtons() {
+    bf->Render();
+    br->Render();
+    bg->Render();
+    bb->Render();
+    bl->Render();
+
+    bplus->Render();
+    bminus->Render();
+
+    bup->Render();
+    bdown->Render();
+    bleft->Render();
+    bright->Render();
+}
+
+void fullRender()
+{
+    bmp->renderBitmap(offset, imgOffset, 1, 1, 1, rotation);
+    bmp->renderHistogram(x0, y0, xf, yf, 1, 1, 1);
+}
+
+void redRender()
+{
+    bmp->renderBitmap(offset, imgOffset, 1, 0, 0, rotation);
+    bmp->renderHistogram(x0, y0, xf, yf, 1, 0, 0);
+}
+
+void greenRender()
+{
+    bmp->renderBitmap(offset, imgOffset, 0, 1, 0, rotation);
+    bmp->renderHistogram(x0, y0, xf, yf, 0, 1, 0);
+}
+
+void blueRender()
+{
+    bmp->renderBitmap(offset, imgOffset, 0, 0, 1, rotation);
+    bmp->renderHistogram(x0, y0, xf, yf, 0, 0, 1);
+}
+
+void monoRender()
+{
+    bmp->renderBitmapMonochrome(offset, imgOffset, rotation);
+    bmp->renderHistogramMonochrome(x0, y0, xf, yf);
+}
+
+//funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis globais
+//Todos os comandos para desenho na canvas devem ser chamados dentro da render().
+//Deve-se manter essa função com poucas linhas de codigo.
+void render()
+{
+   renderButtons();
+
+   if ( opcao == 201 ) rotation = 1;
+   else if ( opcao == 203 ) rotation = 2;
+   else if ( opcao == 200 ) rotation = 3;
+   else if ( opcao == 202 ) rotation = 4;
+
+   if ( opcao == 49 )
+   {
+      fullRender();
+      auxOpcao = opcao;
+   }
+   else if( opcao == 50 ) //50 -> vermelho
+   {
+      redRender();
+      auxOpcao = opcao;
+   }
+   else if( opcao == 51 ) //'3' -> verde
+   {
+      greenRender();
+      auxOpcao = opcao;
+   }
+   else if( opcao == 52 ) //'4' -> azul
+   {
+      blueRender();
+      auxOpcao = opcao;
+   }
+   else if ( opcao == 53 ) {
+      monoRender();
+      auxOpcao = opcao;
+   }
+   else
+   {
+      opcao = auxOpcao;
+
+      if(opcao == 49) fullRender();
+      else if(opcao == 50) redRender();
+      else if(opcao == 51) greenRender();
+      else if(opcao == 52) blueRender();
+      else if(opcao == 53) monoRender();
+   }
+}
+
+//funcao chamada toda vez que uma tecla for pressionada.
+void keyboard(int key)
+{
+   printf("\nTecla: %d" , key);
+   if( key < 204 )
+   {
+      opcao = key;
+   }
+}
+
+//funcao chamada toda vez que uma tecla for liberada
+void keyboardUp(int key)
+{
+   printf("\nLiberou: %d" , key);
+}
+
+//funcao para tratamento de mouse: cliques,  movimentos e arrastos
+void mouse(int button, int state, int wheel, int direction, int x, int y)
+{
+   mouseX = x;
+   mouseY = y;
+
+   if( state == 0 ) //clicou
+   {
+       if(bf->Colidiu(x, y)) opcao = 49;
+       if(br->Colidiu(x, y)) opcao = 50;
+       if(bg->Colidiu(x, y)) opcao = 51;
+       if(bb->Colidiu(x, y)) opcao = 52;
+       if(bl->Colidiu(x, y)) opcao = 53;
+       if(bup->Colidiu(x, y)) rotation = 1;
+       if(bdown->Colidiu(x, y)) rotation = 2;
+       if(bleft->Colidiu(x, y)) rotation = 3;
+       if(bright->Colidiu(x, y)) rotation = 4;
+   }
+
+   //printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+}
+
+int main(void)
+{
+   bmp = new Bmp(".\\gl_1_canvasGlut\\resources\\img1.bmp");
+   bmp->convertBGRtoRGB();
+
+   screenWidth = (2 * offset) + bmp->getWidth() + graphOffset;
+   screenHeight = (2 * offset) + bmp->getHeight();
+
+   imgOffset = screenHeight - bmp->getHeight() - offset;
+
+   x0 = screenWidth + offset - graphOffset;
+   y0 = offset + offset/2;
+   xf = screenWidth - offset;
+   yf = 100 + offset + offset/2;
+
+   startButtons(offset/2, offset/2);
+
+   initCanvas(&screenWidth, &screenHeight, "BMP - Pressione 1, 2, 3, 4 ou 5");
+
+   runCanvas();
+}
